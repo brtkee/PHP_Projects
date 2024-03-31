7c07c8af44739a74bdac995c4362e6075d0d8561
brtkee <150546160+brtkee@users.noreply.github.com>
2024-03-31 17:21:44 +0200

    Create logout.php

---
+++ ForumApp/logout.php
@@ -0,0 +1,18 @@
+<?php 
+    session_start();
+
+    if(!isset($_SESSION['user_id']))
+    {
+        header("Location: index.php");
+        exit();
+    }
+
+    $_SESSION = array();
+
+    session_unset();
+    session_destroy();
+
+    header("Location: index.php");
+    exit();
+
+?>

0636b98575c508eb396b537d9201165dd0242592
brtkee <150546160+brtkee@users.noreply.github.com>
2024-04-25 18:51:15 +0200

    Create index.php

---
+++ Live search using PHP and AJAX/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Live Search</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+        <div class="card-container">
+            <h2 class="card-header">
+                <input type="text" id="query" placeholder="Type Something...">
+            </h2>
+
+            <card class="card-content">
+                
+            </card>
+
+            <footer class="card-footer">
+                Footer
+            </footer>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
